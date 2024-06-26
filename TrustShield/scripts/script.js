3429b5957047c42b03ebb4aa0ab5953257b8bb03
Larissa Böhm Araújo <145381988+lalabohm@users.noreply.github.com>
2024-06-26 10:24:39 -0300

    Update script.js

--- TrustShield/scripts/script.js
+++ TrustShield/scripts/script.js
@@ -2,7 +2,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     event.preventDefault(); // Impede o envio padrão do formulário
 
     // Redireciona para a página index.html na pasta templates após o login
-    window.location.href = '/templates/index.html';
+    window.location.href = 'index.html';
 });
 
 
